45bd8e2a4118e8bf3b7779680da3727944c37710
Vishal KUMAR <imvksb@gmail.com>
2026-02-04 20:33:51 +0530

    fix

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:3000",           // local development
-      "https://www.aspirantcareer.in/",    // production frontend
+      "https://www.aspirantcareer.in",    // production frontend
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
