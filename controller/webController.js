b95a4bdf0c06b7c5d00174289a32439004da9720
Vishal KUMAR <127499392+vishal02376@users.noreply.github.com>
2026-04-08 22:57:43 +0530

    bug fixed

--- controller/webController.js
+++ controller/webController.js
@@ -410,9 +410,20 @@ const eligibilityCheckController = async (req, res) => {
     const andConditions = [
       {
         $or: [
-          { "importantDates.applyEnd": { $gte: today } },
-          { "importantDates.applyEnd": { $exists: false } },
-          { "importantDates.applyEnd": null },
+          { "importantDates.applyStart.tentative": true },
+        // Old schema: applyStart was null
+        { "importantDates.applyStart": null },
+
+        // New schema: applyEnd.date >= today
+        { "importantDates.applyEnd.date": { $gte: today } },
+        // New schema: applyEnd is tentative
+        { "importantDates.applyEnd.tentative": true },
+
+        // Old schema: applyEnd was a plain Date >= today
+        { "importantDates.applyEnd": { $gte: today } },
+        // Old schema: applyEnd didn't exist or was null
+        { "importantDates.applyEnd": { $exists: false } },
+        { "importantDates.applyEnd": null },
         ],
       },
     ];
